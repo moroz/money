78c6e728c3aabbee413038422301df244c0810da
Karol Moroz <k.j.moroz@gmail.com>
2017-09-14 12:07:09 +0200

    Stub specs for Money conversions and comparisons

--- spec/money/money_spec.rb
+++ spec/money/money_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Money::Money do
+
   describe "initialization" do
     context "when given valid params" do
       it "initializes a Money object"
@@ -28,6 +29,74 @@ RSpec.describe Money::Money do
       it "raises ArgumentError" do
 
       end
+    end
+  end
+
+  describe "conversions" do
+    describe '#convert_to' do
+      context "when given Currency object" do
+        context "when the other Currency is same as own" do
+          it "returns self"
+        end
+
+        context "when the other Currency is different" do
+          it 'returns calculated value' do
+
+          end
+        end
+      end
+
+      context "when given other type of object" do
+        it "raises ArgumentError" do
+        end
+      end
+    end
+  end
+
+  describe "comparisons" do
+    describe "==" do
+      context "when the other object has same amount and currency" do
+        it "returns true" do
+
+        end
+      end
+
+      context "when the other object is in other currency" do
+        context "when equal after conversion" do
+          it "returns true" do
+
+          end
+        end
+
+        context "when unequal after conversion" do
+          it "returns false" do
+
+          end
+        end
+      end
+    end
+
+    describe "!=" do
+      context "when the other object has same amount and currency" do
+        it "returns false" do
+
+        end
+      end
+
+      context "when the other object is in other currency" do
+        context "when equal after conversion" do
+          it "returns false" do
+
+          end
+        end
+
+        context "when unequal after conversion" do
+          it "returns true" do
+
+          end
+        end
+      end
+
     end
   end
 end
