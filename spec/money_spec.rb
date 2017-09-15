807954ad763fb3c5f6ea923e501b5a532898c11b
Karol Moroz <k.j.moroz@gmail.com>
2017-09-15 15:23:26 +0200

    Add specs for Money conversion and comparisons

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe Money::Money do
       status: 200,
       body: json_string(json_file('eur'))
     })
+    stub_request(:get, 'https://api.fixer.io/latest?base=USD').to_return({
+      status: 200,
+      body: json_string(json_file('usd'))
+    })
   end
 
   describe "initialization" do
@@ -49,21 +53,39 @@ RSpec.describe Money::Money do
   end
 
   describe "conversions" do
+    let(:fifty_usd) { Money.new(50, usd) }
+
     describe '#convert_to' do
       context "when given Currency object" do
         context "when the other Currency is same as own" do
-          it "returns self"
+          it "returns equal object" do
+            fifty_usd_in_usd = fifty_usd.convert_to(usd)
+            expect(fifty_usd_in_usd).to eq(fifty_usd)
+          end
         end
 
         context "when the other Currency is different" do
-          it 'returns calculated value' do
+          let(:fifty_usd_in_eur) { fifty_usd.convert_to(eur) }
 
+          it 'returns instance of Money' do
+            expect(fifty_usd_in_eur).to be_instance_of(Money)
+          end
+
+          # Conversion rates in the fixtures:
+          # USD-EUR: 0.8414
+          it 'calculates the amounts correctly' do
+            expect(fifty_usd_in_eur.amount).to eq(BigDecimal.new(42.07, 4))
+          end
+
+          it 'returned object has the valid currency code' do
+            expect(fifty_usd_in_eur.currency_code).to eq('EUR')
           end
         end
       end
 
       context "when given other type of object" do
         it "raises ArgumentError" do
+          expect { fifty_usd.convert_to('EUR') }.to raise_exception(ArgumentError)
         end
       end
     end
@@ -73,46 +95,29 @@ RSpec.describe Money::Money do
     describe "==" do
       context "when the other object has same amount and currency" do
         it "returns true" do
-
+          twenty_eur = Money.new(20, eur)
+          another_twenty_eur = Money.new(20, eur)
+          expect(another_twenty_eur).to eq(twenty_eur)
         end
       end
 
       context "when the other object is in other currency" do
         context "when equal after conversion" do
           it "returns true" do
-
+            fifty_usd = Money.new(50, usd)
+            forty_two_eur = Money.new(42.07, eur)
+            expect(forty_two_eur).to eq(fifty_usd)
           end
         end
 
         context "when unequal after conversion" do
           it "returns false" do
-
+            twenty_usd = Money.new(20, usd)
+            thousand_eur = Money.new(1000, eur)
+            expect(twenty_usd).not_to eq(thousand_eur)
           end
         end
       end
-    end
-
-    describe "!=" do
-      context "when the other object has same amount and currency" do
-        it "returns false" do
-
-        end
-      end
-
-      context "when the other object is in other currency" do
-        context "when equal after conversion" do
-          it "returns false" do
-
-          end
-        end
-
-        context "when unequal after conversion" do
-          it "returns true" do
-
-          end
-        end
-      end
-
     end
   end
 end
